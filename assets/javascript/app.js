80f7943fbc25ab8f7f927ea303cf981df2bc5267
Cruzerman <cbmcruzerman@hotmail.com>
2019-05-28 20:47:55 -0700

    changes to get google maps to show up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,3 +15,25 @@ function renderAirports() {
   
   }
 renderAirports();
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.5155, lng: -122.6793},
+    zoom: 13
+    
+  });
+
+  var marker = new google.maps.Marker({
+      position:{lat: 45.5155, lng: -122.6793},
+      map:map
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+      content: '<h4>Portland, Oregon</h4>'
+  });
+
+  marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+  });
+}
